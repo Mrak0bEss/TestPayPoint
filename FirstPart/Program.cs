2bb2db8e7c565abeae27833f80d4a25f253937b3
Mrak0bEss <106889859+Mrak0bEss@users.noreply.github.com>
2023-04-26 21:03:47 +0300

    Добавьте файлы проекта.

---
+++ FirstPart/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TronNet;
+using TronNet.Accounts;
+using TronNet.Contracts;
+using TronNet.Protocol;
+using TronNet.Crypto;
+using TronNet.ABI;
+namespace FirstPart
+{
+    class Program
+    {
+        public class AccountData
+        {
+            private TronECKey key;
+            private TronAccount acc;
+            public AccountData()
+            {
+                TronNetwork netw = new TronNetwork();
+                netw = TronNetwork.MainNet;
+                key = TronECKey.GenerateKey(netw);
+                acc = new TronAccount(key);
+            }
+            public void printAccountData()
+            {
+                Console.WriteLine("Adress: " + acc.Address);
+                Console.WriteLine("Private key: " + acc.PrivateKey);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            AccountData account = new AccountData();
+            account.printAccountData();
+        }
+    }
+}
